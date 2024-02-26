2a9e3f1722fd6af36a7b8ac2265f6ec065b3a98a
Stat3132 <starxstar1016@gmail.com>
2024-02-26 11:13:14 -0700

    test 1

--- src/main/java/org/example/UTIL/Card.java
+++ src/main/java/org/example/UTIL/Card.java
@@ -1,17 +1,62 @@
 package org.example.UTIL;
 
+import javax.xml.namespace.QName;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Card {
-    /**
-     * This should be where both card value and suit converge to make cards. There should only be 52 cards and here it will
-     * just set an organized card number going A,2,3,4,5,6,7,8,9,10.J,Q,K. Then in deck of cards it is randomized, or maybe
-     * we can remove a class and do all functionality in one class making it so our code is DRY and KISS.
-     * @param cardValue
-     * @param Suit
-     */
-    public Card(Array cardValue, Array Suit) {
+   protected CardValue[] allCardValues = new CardValue[13];
+   protected Suit[] allSuits = new Suit[4];
+   String face;
+   int suit;
 
+   public Card(CardValue face, Suit suit){
+
+   }
+   public void populateDeck(){
+       int suitCounter = 0;
+       for (int i = 0; i < 100 ; i++) {
+           Card newCard = new Card(allCardValues[i],allSuits[suitCounter]);
+           System.out.println(newCard);
+           if (suitCounter == 3 && i == 12){
+              return;
+           }
+           if (i == 12){
+               i = 0;
+               suitCounter++;
+           }
+       }
+   }
+    //Makes array of cardValues and Suits
+    public void makingEnumArrays() {
+        CardValue[] intForCardValues = CardValue.values();
+        Suit[] intForSuits = Suit.values();
+        for (int i = 0; i < allCardValues.length; i++) {
+            if (allCardValues[i] == null) {
+                allCardValues[i] = intForCardValues[i];
+            }
+            if (i == intForCardValues.length - 1) {
+                break;
+            }
+        }
+        for (int i = 0; i < allSuits.length; i++) {
+            if (allSuits[i] == null) {
+                allSuits[i] = intForSuits[i];
+            }
+            if (i == allSuits.length - 1) {
+                break;
+            }
+        }
+        System.out.println(allCardValues[12]);
+        System.out.println(allSuits[3]);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "face='" + face + '\'' +
+                ", suit=" + suit +
+                '}';
     }
 }
