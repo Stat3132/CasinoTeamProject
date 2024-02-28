8fd4d02a4aef66a1fc3a08964cb4aa7d50ca1d9d
Stat3132 <starxstar1016@gmail.com>
2024-02-27 19:12:48 -0700

    Updated odds for horses ensuring that it contributes to a horse winning but does not guarentee a victory. Added comments to ProbabiltyForValue.

--- src/main/java/org/example/UTIL/ProbabilityForValue.java
+++ src/main/java/org/example/UTIL/ProbabilityForValue.java
@@ -24,6 +24,7 @@ public class ProbabilityForValue {
         }
     }
 
+    //Randomizing odds and if odds are used removing them from an array for no overlapping odds.
     public int randomOddValue() {
         Random rand = new Random();
         int randomOdd = rand.nextInt(0, arrayListOfOdds.size());
@@ -37,34 +38,66 @@ public class ProbabilityForValue {
         return randomOdd;
     }
 
+    // Making sure odds contributes to the horses winning but still does not guarantee the win.
+    public int makingMathematicalOdds(Horse racerHorse) {
+        if (racerHorse == null) {
+            return 0;
+        }
+        if (racerHorse.getHorseOdds() <= 100 && racerHorse.getHorseOdds() >= 80) {
+            return randomValues(0, 2);
+        }
+        if (racerHorse.getHorseOdds() <= 79 && racerHorse.getHorseOdds() >= 60) {
+            return randomValues(0, 6);
+        }
+        if (racerHorse.getHorseOdds() <= 59 && racerHorse.getHorseOdds() >= 40) {
+            return randomValues(0, 9);
+        }
+        if (racerHorse.getHorseOdds() <= 39 && racerHorse.getHorseOdds() >= 30) {
+            return randomValues(0, 12);
+        }
+        if (racerHorse.getHorseOdds() <= 29 && racerHorse.getHorseOdds() >= 20) {
+            return randomValues(0, 15);
+        }
+        if (racerHorse.getHorseOdds() <= 19 && racerHorse.getHorseOdds() >= 10) {
+            return randomValues(0, 18);
+        }
+        if (racerHorse.getHorseOdds() <= 9 && racerHorse.getHorseOdds() >= 0) {
+            return randomValues(0, 21);
+        }
+        return 0;
+    }
+
     //Odds do not fully determine winner this method is the one that will determine the winner, with one more "random" check.
-    //TODO: Find a way to loop this switch until a horse has a placement and then removing that placement from the switch statement so that only the remaining slots can be chosen.
-    //TODO: PSEUDO CODE NO FULL IMPLEMENTATION
-    //FIXME:\
+
+    /**
+     * This method uses the horses odds and gives it one more random "spin" to truly randomize the odds.
+     *
+     * @param finalLineUp This is the lineup of horses that have already been ordered from the least odds to the greatest odds
+     * @return This returns a temporary array that now becomes the new positionings for the final horses.
+     */
     public Horse[] oddsOfOdds(Horse[] finalLineUp) {
         Horse[] tempPlacement = new Horse[7];
         int incrementingTempPlacement = 0;
         for (int i = 0; i < 8; i++) {
-            if (i == 7){
+            if (i == 7) {
                 i = 0;
             }
             switch (incrementingTempPlacement) {
                 case 0:
-                    lastCheck = randomValues(0, 7);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
                 case 1:
-                    lastCheck = randomValues(0, 6);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
                 case 2:
-                    lastCheck = randomValues(0, 5);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
                 case 3:
-                    lastCheck = randomValues(0, 4);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
                 case 4:
-                    lastCheck = randomValues(0, 3);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
                 case 5:
-                    lastCheck = randomValues(0, 2);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
                 case 6:
-                    lastCheck = randomValues(0, 1);
+                    lastCheck = makingMathematicalOdds(finalLineUp[i]);
             }
-
             if (tempPlacement[6] != null) {
                 break;
             }
