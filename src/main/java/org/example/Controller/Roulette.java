d830d4f6326a27a983c0ac185b5e0cd6d277fe7e
Ethan Townsend <ethantownsend73@gmail.com>
2024-02-27 19:11:49 -0700

    NEW UI AND CONTROLLER FINISHED! added comments

--- src/main/java/org/example/Controller/Roulette.java
+++ src/main/java/org/example/Controller/Roulette.java
@@ -75,7 +75,7 @@ public class Roulette implements Casino {
 
 
         }
-        UI.displayRouletteFinalMoney(currentPlayer);
+        UI.displayFinalMoney(currentPlayer,2);
         return null;
     }
 
